ea64a8ccd83fa83b700277f4c1c16dbdd83b0664
Henri <henri@la-quincaillerie.fr>
2021-03-17 15:14:40 +0100

    Remoove useless Forum settings

--- index.php
+++ index.php
@@ -6,13 +6,11 @@ require_once dirname(__FILE__) . '/includes/top.inc.php';
 
 if (isset($_GET['page'])) {
     $pageFilename = dirname(__FILE__) . '/pages/' . $_GET['page'] . '.content.php';
-    $pageFilenameForum = dirname(__FILE__) . '/pages/forum/' . $_GET['page'] . '.content.php';
+
 
     if (file_exists($pageFilename)) {
         require_once $pageFilename;
 
-    } elseif (file_exists($pageFilenameForum)) {
-        require_once $pageFilenameForum;
     } else {
         require_once dirname(__FILE__) . '/pages/404.content.php';
     }
