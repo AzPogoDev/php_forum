186ff0c1da808b1d4965b97cf301d0fd41774962
Henri <henri@la-quincaillerie.fr>
2021-03-17 15:15:16 +0100

    Adding Admin Menu

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -3,13 +3,11 @@
         <div class="row">
             <div class="col-md-12">
                 <nav class="navi navbar" role="navigation">
-                    <a href="#" class="logo">
+                    <a href="/?page=home" class="logo">
                         H Forum
                     </a>
                     <ul class="menu nav nav-pills navbar-right">
                         <li class="menu__item is-itemHov"><a href="/?page=home">HOME</a></li>
-                        <li class="menu__item is-itemHov"><a href="/?page=list-articles">ARTICLES</a></li>
-                        <li class="menu__item is-itemHov"><a href="/?page=forum">FORUM</a></li>
                         <li class="menu__item is-itemHov"><a href="/?page=admin">ADMIN</a></li>
                     </ul>
                 </nav>
