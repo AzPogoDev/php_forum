cd99084b615464c47a4631b64ece0c898a67202a
Henri <henri@la-quincaillerie.fr>
2021-03-18 11:42:23 +0100

    Delete reply

--- pages/single-subject.content.php
+++ pages/single-subject.content.php
@@ -17,7 +17,7 @@ $querybis->execute([
 $subjectId = $_GET['subid'];
 
 if (isset($_POST['replyForm'])) {
-    if ( isset($_POST['content']) && !empty($_POST['content'])) {
+    if (isset($_POST['content']) && !empty($_POST['content'])) {
 
         $queryInsert = $database->prepare('INSERT INTO `reply`(`author`, `content`, `subjectId`) VALUES (:author,:content,:subid)');
         $queryInsert->execute([
@@ -30,48 +30,84 @@ if (isset($_POST['replyForm'])) {
     }
 }
 
+
+if (isset($_POST['replyToDelete'])) {
+    if (isset($_POST['replyIdToDelete']) && !empty($_POST['replyIdToDelete'])) {
+
+        $queryInsert = $database->prepare('DELETE FROM `reply` WHERE `reply`.`id` = :replyIdToDelete');
+
+        $queryInsert->execute([
+            'replyIdToDelete' => $_POST['replyIdToDelete']
+        ]);
+
+        header("Location: ./?page=single-subject&subid=$subjectId");
+    }
+}
+
 ?>
 <div class="wrapper">
 
     <?php while (($q = $querybis->fetch())) { ?>
 
-        <div class="subject__content">
+        <div class="subject__content" style="min-height: 15vh;margin-top: 20vh">
             <div class="post__status"><?= $q['status'] ?></div>
-            <h1><h1 class="text-center"><?= $q['title'] ?></h1></h1>
-            <p><?= $q['content'] ?><p></p></p>
+            <h1><h1 class="text-center text-decoration-underline"><?= $q['title'] ?></h1></h1>
+            <p class="mt-3 text-center"><?= $q['content'] ?><p></p></p>
         </div>
+
+
+        <div class="reply__content my-5">
+            <!--        <h2 class="my-2" style="font-size: 2rem">Réponse :</h2>-->
+            <?php while (($qb = $query->fetch())) { ?>
+
+                <div class="reponse__single my-4">
+                    <div class="reponse__author d-flex justify-content-between">
+                        <h2>Auteur : <?= $qb['author'] ?></h2>
+                        <?php if (isset($_SESSION['user'])) : ?>
+                            <form action="/?page=single-subject&subid=<?= $subjectId ?>" method="POST">
+
+                                <input type="hidden" name="replyIdToDelete" value="<?= $qb['id']; ?>">
+                                <button type="submit" name="replyToDelete" class="my-2 btn btn-danger">X
+                                </button>
+                            </form>
+                        <?php endif ?>
+                    </div>
+                    <div class="reponse__content py-3" style="background: #7e7b7b; color: black">
+                        <p><?= $qb['content'] ?></p>
+                    </div>
+                </div>
+
+            <?php } ?>
+        </div>
+
+        <?php  if ( $q['status'] == 'Ouvert') : ?>
+
+        <div class="reply__form">
+            <h2 class="my-2" style="font-size: 2rem">Ajouter une réponse :</h2>
+            <form action="/?page=single-subject&subid=<?= $subjectId ?>" method="POST">
+                <div class="form-group">
+                    <label for="author">Auteur</label>
+
+                    <input type="text" class="form-control" id="author" name="author" placeholder=" Auteur">
+
+                </div>
+
+
+                <div class="form-group">
+                    <label for="content">Contenue</label>
+                    <textarea type="text" class="form-control" id="content" name="content"
+                              placeholder=" Contenue"></textarea>
+                </div>
+                <button type="submit" name="replyForm" class="my-2 btn btn-primary">Ajouter</button>
+            </form>
+        </div>
+
+        <?php else : ?>
+
+            <h2 class="my-2" style="font-size: 2rem">Sujet clos</h2>
+
+        <?php endif; ?>
+
     <?php } ?>
-    <div class="reply__content my-5">
-        <h2 class="my-2" style="font-size: 2rem">Réponse :</h2>
-        <?php while (($qb = $query->fetch())) { ?>
-
-            <div class="reponse__single">
-                <div class="reponse__author">
-                    <h2><?= $qb['author'] ?></h2>
-                </div>
-                <div class="reponse__content">
-                    <p><?= $qb['content'] ?></p>
-                </div>
-            </div>
-
-        <?php } ?>
-    </div>
-    <div class="reply__form">
-        <h2 class="my-2" style="font-size: 2rem">Ajouter une réponse :</h2>
-        <form action="/?page=single-subject&subid=<?= $subjectId?>" method="POST">
-            <div class="form-group">
-                <label for="author">Author</label>
-                <input type="text" class="form-control" id="author" name="author" placeholder="Auteur">
-            </div>
-
-            <div class="form-group">
-                <label for="content">Content</label>
-                <textarea type="text" class="form-control" id="content" name="content" placeholder="content"></textarea>
-            </div>
-            <button type="submit" name="replyForm" class="my-2 btn btn-primary">Submit</button>
-        </form>
-    </div>
-
-
 </div>
 
