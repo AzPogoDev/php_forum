0c230ff0d3a28dc41a212caa0e54858a7c878861
Henri <henri@la-quincaillerie.fr>
2021-03-18 11:40:08 +0100

    Adding Admin ROles

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -8,13 +8,26 @@
                     </a>
                     <ul class="menu nav nav-pills navbar-right">
                         <li class="menu__item is-itemHov"><a href="/?page=home">HOME</a></li>
-                        <li class="menu__item is-itemHov"><a href="/?page=admin">ADMIN</a></li>
+                        <?php if (isset($_SESSION['user'])) : ?>
+                            <li class="menu__item is-itemHov"><a href="/?page=admin">ADMIN</a></li>
+                        <?php endif; ?>
+                        <?php if (!isset($_SESSION['user'])) : ?>
+                            <li class="menu__item is-itemHov"><a href="/?page=login">CONNEXION</a></li>
+                        <?php endif; ?>
                     </ul>
+                    <?php if (isset($_SESSION['user'])) : ?>
+                    <form action="/?page=admin" method="POST">
+                        <input type="hidden" name="userId" value=UserId">
+                        <button type="submit" name="disconnect" class="my-2 btn btn-danger">Se déconnecter de : <?=
+                            $_SESSION['user']; ?>
+
+                        </button>
+                        <?php endif; ?>
+                    </form>
                 </nav>
             </div>
         </div>
     </div>
 </header>
 <div class="website-content">
-
 
