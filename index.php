52fcbd765f6c597b57c7223426251fd66c1b53ed
Henri <henri@la-quincaillerie.fr>
2021-03-18 11:41:24 +0100

    Fixing admin roles page access

--- index.php
+++ index.php
@@ -4,9 +4,16 @@ session_start();
 
 require_once dirname(__FILE__) . '/includes/top.inc.php';
 
+if (empty($_SESSION['user']) ){
+
+    if($_GET['page'] == "admin" && !isset($_SESSION['user'])){
+        header("Location: ./?page=home");
+        die();
+    }
+}
+
 if (isset($_GET['page'])) {
     $pageFilename = dirname(__FILE__) . '/pages/' . $_GET['page'] . '.content.php';
-
 
     if (file_exists($pageFilename)) {
         require_once $pageFilename;
@@ -14,7 +21,7 @@ if (isset($_GET['page'])) {
     } else {
         require_once dirname(__FILE__) . '/pages/404.content.php';
     }
-} else {
+}  else {
     require_once dirname(__FILE__) . '/pages/home.content.php';
 }
 
